29086191cdad9ed3bcda562f3c574399f7e73538
Warden-labs <ezhitnikovich@gmail.com>
2020-10-16 23:08:27 +0300

    first commit

---
+++ Lab1/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lab1
+{
+    class MainLab1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
