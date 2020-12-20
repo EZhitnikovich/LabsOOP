8c20e8de16cec9a1e1490ed850c9ccd6626e852f
EZhitnikovich <ezhitnikovich@gmail.com>
2020-12-20 17:43:44 +0300

    add struct and operations

--- Lab3/DataTypesTester.cs
+++ Lab3/DataTypesTester.cs
@@ -1,64 +1,129 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static Lab3.Program;
 
 namespace Lab3
 {
-    class DataTypesTester<T> : IDataTypeTester
+    class DataTypesTester<T> : IDataTypeTester where T : unmanaged
     {
-        public T Value1 { get; set; }
-        public T Value2 { get; set; }
+        private Func<string>[] operations;
 
-        public DataTypesTester(T v1, T v2)
+        TestStruct<T> ExampleStruct { get; set; }
+
+        public DataTypesTester(TestStruct<T> testSturct)
         {
-            Value1 = v1;
-            Value2 = v2;
+            ExampleStruct = testSturct;
         }
 
         public Func<string>[] GetOperatiounsResults()
         {
+
+            var Value1 = ExampleStruct.value1;
+            var Value2 = ExampleStruct.value2;
             OutputService.ShowMessage(typeof(T).Name);
-            Func<string>[] operations = new Func<string>[]
+            int[] numbers = new[] { 0, 10, 20, 30, 40, 50 };
+            unsafe
             {
-                () => $"{Value1} + {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 + v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} - {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 - v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} * {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 * v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} / {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 / v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} % {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 & v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} & {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 & v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} | {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 | v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} && {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 && v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} || {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 || v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} ?? {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ?? v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} ^ {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ^ v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} > {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 > v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} < {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 < v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} <= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 <= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} >= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 >= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} == {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 == v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} != {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 != v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} >> {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 >> v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} << {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 << v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} = {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 = v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} += {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 += v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} -= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 -= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} *= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 *= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} /= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 /= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} %= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 %= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} &= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 &= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} |= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 |= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} ??= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ??= v2).Invoke(Value1, Value2)}",
-                () => $"{Value1} ^= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ^= v2).Invoke(Value1, Value2)}",
-                () => $"{Value2}++ = {new Func<dynamic, dynamic>((v1) => v1++).Invoke(Value2)}",
-                () => $"++{Value2} = {new Func<dynamic, dynamic>((v1) => ++v1).Invoke(Value2)}",
-                () => $"{Value2}-- = {new Func<dynamic, dynamic>((v1) => v1--).Invoke(Value2)}",
-                () => $"--{Value2} = {new Func<dynamic, dynamic>((v1) => --v1).Invoke(Value2)}",
-                () => $"~{Value2} = {new Func<dynamic, dynamic>((v1) => ~v1).Invoke(Value2)}",
-                () => $"!{Value2} = {new Func<dynamic, dynamic>((v1) => !v1).Invoke(Value2)}",
-                () => $"{Value1} > {Value2} ? true : false = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 > v2 ? true : false).Invoke(Value1, Value2)}",
-            };
+                TestStruct<T> exStruct = new TestStruct<T>();
+                TestStruct<T>* pointer = &exStruct;
+                operations = new Func<string>[]
+                {
+                    () => $"{Value1} + {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 + v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} - {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 - v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} * {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 * v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} / {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 / v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} % {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 & v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} & {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 & v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} | {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 | v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} && {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 && v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} || {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 || v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} ?? {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ?? v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} ^ {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ^ v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} > {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 > v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} < {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 < v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} <= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 <= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} >= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 >= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} == {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 == v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} != {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 != v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} >> {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 >> v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} << {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 << v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} = {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 = v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} += {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 += v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} -= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 -= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} *= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 *= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} /= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 /= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} %= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 %= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} &= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 &= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} |= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 |= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} ??= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ??= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} ^= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 ^= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} >>= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 >>= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value1} <<= {Value2} = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 <<= v2).Invoke(Value1, Value2)}",
+                    () => $"{Value2}++ = {new Func<dynamic, dynamic>((v1) => v1++).Invoke(Value2)}",
+                    () => $"++{Value2} = {new Func<dynamic, dynamic>((v1) => ++v1).Invoke(Value2)}",
+                    () => $"{Value2}-- = {new Func<dynamic, dynamic>((v1) => v1--).Invoke(Value2)}",
+                    () => $"--{Value2} = {new Func<dynamic, dynamic>((v1) => --v1).Invoke(Value2)}",
+                    () => $"~{Value2} = {new Func<dynamic, dynamic>((v1) => ~v1).Invoke(Value2)}",
+                    () => $"!{Value2} = {new Func<dynamic, dynamic>((v1) => !v1).Invoke(Value2)}",
+                    () => $"{Value1} > {Value2} ? true : false = {new Func<dynamic, dynamic, dynamic>((v1, v2) => v1 > v2 ? true : false).Invoke(Value1, Value2)}",
+                    () => $"+{Value2} = {new Func<dynamic, dynamic>((v1) => +v1).Invoke(Value2)}",
+                    () => $"-{Value2} = {new Func<dynamic, dynamic>((v1) => -v1).Invoke(Value2)}",
+                    () => $"{Value2}! = {new Func<dynamic, dynamic>((v1) => v1!).Invoke(Value2)}",
+                    () => $"(byte){Value2} = {new Func<dynamic, dynamic>((v1) => (byte)v1).Invoke(Value2)}",
+                    () => $"(short){Value2} = {new Func<dynamic, dynamic>((v1) => (short)v1).Invoke(Value2)}",
+                    () => $"(char){Value2} = {new Func<dynamic, dynamic>((v1) => (char)v1).Invoke(Value2)}",
+                    () => $"(int){Value2} = {new Func<dynamic, dynamic>((v1) => (int)v1).Invoke(Value2)}",
+                    () => $"(long){Value2} = {new Func<dynamic, dynamic>((v1) => (long)v1).Invoke(Value2)}",
+                    () => $"(float){Value2} = {new Func<dynamic, dynamic>((v1) => (float)v1).Invoke(Value2)}",
+                    () => $"(double){Value2} = {new Func<dynamic, dynamic>((v1) => (double)v1).Invoke(Value2)}",
+                    () => $"(decimal){Value2} = {new Func<dynamic, dynamic>((v1) => (decimal)v1).Invoke(Value2)}",
+                    () => $"(bool){Value2} = {new Func<dynamic, dynamic>((v1) => (bool)v1).Invoke(Value2)}",
+                    () => $"{Value2} is byte = {new Func<dynamic, dynamic>((v1) => v1 is byte).Invoke(Value2)}",
+                    () => $"{Value2} is short = {new Func<dynamic, dynamic>((v1) => v1 is short).Invoke(Value2)}",
+                    () => $"{Value2} is char = {new Func<dynamic, dynamic>((v1) => v1 is char).Invoke(Value2)}",
+                    () => $"{Value2} is int = {new Func<dynamic, dynamic>((v1) => v1 is int).Invoke(Value2)}",
+                    () => $"{Value2} is long = {new Func<dynamic, dynamic>((v1) => v1 is long).Invoke(Value2)}",
+                    () => $"{Value2} is float = {new Func<dynamic, dynamic>((v1) => v1 is float).Invoke(Value2)}",
+                    () => $"{Value2} is double = {new Func<dynamic, dynamic>((v1) => v1 is double).Invoke(Value2)}",
+                    () => $"{Value2} is decimal = {new Func<dynamic, dynamic>((v1) => v1 is decimal).Invoke(Value2)}",
+                    () => $"{Value2} is bool = {new Func<dynamic, dynamic>((v1) => v1 is bool).Invoke(Value2)}",
+                    () => $"{Value1}..{Value2} = {GetRangeResultString<T>((v1, v2, arr) => arr[v1..v2], Value1, Value2, numbers)}",
+                    () => $"{Value1}..^{Value2} = {GetRangeResultString<T>((v1, v2, arr) => arr[v1..^v2], Value1, Value2, numbers)}",
+                    () => $"^{Value1}..{Value2} = {GetRangeResultString<T>((v1, v2, arr) => arr[^v1..v2], Value1, Value2, numbers)}",
+                    () => $"^{Value1}..^{Value2} = {GetRangeResultString<T>((v1, v2, arr) => arr[^v1..^v2], Value1, Value2, numbers)}",
+                    () => $"..^{Value2} = {GetRangeResultString<T>((v2, v1, arr) => arr[..^v2], Value1, Value2, numbers)}",
+                    () => $"^{Value2}.. = {GetRangeResultString<T>((v2, v1, arr) => arr[^v2..], Value1, Value2, numbers)}",
+                    () => $"..{Value2} = {GetRangeResultString<T>((v2, v1, arr) => arr[..v2], Value1, Value2, numbers)}",
+                    () => $"{Value2}.. = {GetRangeResultString<T>((v2, v1, arr) => arr[v2..], Value1, Value2, numbers)}",
+                    () => $"sizeof(T) = {sizeof(T)}",
+                    () => $"typeof(T) = {typeof(T)}",
+                    () => $"default(T) = {default(T)}",
+                    () => $"&pointer = {new IntPtr(pointer)}",
+                    () => $"*pointer = {*pointer}",
+                    () => $"pointer->ToString() = {pointer->ToString()}",
+                };
+            }
 
             return operations;
+        }
+
+        private string GetRangeResultString<T>(Func<dynamic, dynamic, int[], int[]> func, T v1, T v2, int[] arr)
+        {
+            var resArr = RunDelegate(func, v1, v2, arr);
+            return resArr is null ? "Operation not supported" : string.Join(',', resArr);
+        }
+
+        private int[] RunDelegate<T>(Func<T, T, int[], int[]> func, T v1, T v2, int[] arr)
+        {
+            try
+            {
+                return func.Invoke(v1, v2, arr);
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,18 +6,24 @@ namespace Lab3
 {
     class Program
     {
+        public struct TestStruct<T>
+        {
+            public T value1;
+            public T value2;
+        }
+
         static void Main(string[] args)
         {
             List<IDataTypeTester> dataTypeTesters = new List<IDataTypeTester>
             {
-                new DataTypesTester<bool>(true, false),
-                new DataTypesTester<byte>(1, 102),
-                new DataTypesTester<short>(5, 2),
-                new DataTypesTester<int>(2, 4),
-                new DataTypesTester<long>(6, 2),
-                new DataTypesTester<float>(6.1f, 2.4f),
-                new DataTypesTester<double>(5.6, 2.1),
-                new DataTypesTester<char>('c', 'b')
+                new DataTypesTester<bool>(new TestStruct<bool>(){value1 = true, value2 = false }),
+                new DataTypesTester<byte>(new TestStruct<byte>(){value1 = 1, value2 = 3 }),
+                new DataTypesTester<short>(new TestStruct<short>(){value1 = 1, value2 = 3 }),
+                new DataTypesTester<int>(new TestStruct<int>(){value1 = 1, value2 = 3 }),
+                new DataTypesTester<long>(new TestStruct<long>(){value1 = 152L, value2 = 325L }),
+                new DataTypesTester<float>(new TestStruct<float>(){value1 = 1.5f, value2 = 3.523f }),
+                new DataTypesTester<double>(new TestStruct<double>(){value1 = 1.5, value2 = 3.234 }),
+                new DataTypesTester<char>(new TestStruct<char>(){value1 = '4', value2 = 'h' })
             };
 
             foreach(var type in dataTypeTesters)
